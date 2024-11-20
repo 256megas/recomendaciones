27339afdb76fc6e527bea611b3b3502a5e0744f3
Antonio López L <256megas@gmail.com>
2024-11-20 11:38:04 +0100

    Hacemos muestra de 15000 datos

--- recomendation.py
+++ recomendation.py
@@ -15,8 +15,24 @@ df = pd.read_csv('./recomendationEngine/dataset/BX-Books.csv',
 # on_bad_lines omitira las lineas con error
 # on_bad_lines='skip',
 
-print(df.head())
 # Cinco primeros datos
-print("************")
-print(df.info())
+print(df.head())
+print("************************************************")
 # Información de las cabeceras
+print(df.info())
+print("************************************************")
+# Hay duplicados ? Los borramos
+print(df.duplicated(subset='Book-Title').sum())
+df = df.drop_duplicates(subset='Book-Title')
+print(df.duplicated(subset='Book-Title').sum())
+print("************************************************")
+# Cojemos una muestra de 15000 libros
+sample_size = 15000
+# Hacemos una muestra
+# replace=false No permite que la muestra se coja de la misma tupla mas de una vez
+# random_state es la semilla
+df = df.sample(n=sample_size, replace=False, random_state=490)
+df = df.reset_index()
+df = df.drop('index', axis=1)
+print("************************************************")
+print(df.head())
